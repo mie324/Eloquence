c4864d4efd790eca2ed4e32125e29e1bb8c9891e
Sahil Narula <sahiln123@gmail.com>
2019-03-04 15:00:59 -0500

    finished tts and sst

--- app/src/main/java/com/example/urmi/eloquence/MainActivity.java
+++ app/src/main/java/com/example/urmi/eloquence/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.urmi.eloquence;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.cloud.texttospeech.v1.AudioConfig;
 import com.google.cloud.texttospeech.v1.AudioEncoding;
@@ -24,21 +27,27 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] wordList = new String[]{ "Love", "Hate", "Forward" };
+    private WordsList WordsUtility;
+    Random r = new Random();
+    private int currentWordIndex;
+    private String toSpeak;
     private TextToSpeech t;
+    private final int REQUEST_SPEECH_RECOGNIZER = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button tts_click = (Button) findViewById(R.id.tts_button);
+        WordsUtility = new WordsList();
 
-        Button stt_click = (Button)findViewById(R.id.stt_button);
+        Button tts_click = findViewById(R.id.tts_button);
+        Button stt_click = findViewById(R.id.stt_button);
 
         t = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -52,11 +61,49 @@ public class MainActivity extends AppCompatActivity {
         tts_click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String toSpeak = "Say the word, " + wordList[0];
+                currentWordIndex = r.nextInt(WordsUtility.getWordsLength());
+                toSpeak = "Say the word, " + WordsUtility.getWordAt(currentWordIndex);
                 t.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
             }
         });
 
+        stt_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSpeechRecognizer();
+            }
+        });
+
     }
+
+    private void startSpeechRecognizer() {
+        Intent intent = new Intent
+                (RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, toSpeak);
+        startActivityForResult(intent, REQUEST_SPEECH_RECOGNIZER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_SPEECH_RECOGNIZER) {
+            if (resultCode == RESULT_OK) {
+                List<String> results = data.getStringArrayListExtra
+                        (RecognizerIntent.EXTRA_RESULTS);
+                String mAnswer = results.get(0);
+
+                if (mAnswer.toLowerCase().indexOf(WordsUtility.getWordAt(currentWordIndex)) > -1)
+                    Toast.makeText(MainActivity.this, "You answered '" + mAnswer.toUpperCase() + "', which is correct.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "You answered '" + mAnswer.toUpperCase() + "', which is incorrect.", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
 }
 
