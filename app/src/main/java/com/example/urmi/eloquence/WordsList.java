61002d2fc02bf4221b1164ebc90867665f2678e6
Sahil Narula <sahiln123@gmail.com>
2019-03-09 10:22:57 -0500

    new func to get test words

--- app/src/main/java/com/example/urmi/eloquence/WordsList.java
+++ app/src/main/java/com/example/urmi/eloquence/WordsList.java
@@ -1,19 +1,35 @@
 package com.example.urmi.eloquence;
 
-public class WordsList {
-    private String[] wordsList = new String[] {
-            "please", "bus", "need"
-    };
+import android.annotation.TargetApi;
+import android.os.Build;
 
-    public String[] getAllWords () {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class WordsList {
+    private List<String> wordsList = new ArrayList<String>(Arrays.asList("please", "bus", "need"));
+
+    public List<String> getAllWords () {
         return wordsList;
     }
 
     public String getWordAt (int index) {
-        return wordsList[index];
+        return wordsList.get(index);
     }
 
     public int getWordsLength () {
-        return wordsList.length;
+        return wordsList.size();
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public List<String> getTestWords (int num) {
+        List<String> words = new ArrayList<String>();
+        int[] rands = new Random().ints(0, wordsList.size()).limit(num).toArray();
+        for (int n: rands) {
+            words.add(wordsList.get(n));
+        }
+        return words;
     }
 }
